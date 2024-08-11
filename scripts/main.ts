eb568a5fbd114dd7f87098ac03555183cdcd703f
William Wands Jr <wwands@thefallenmoon.com>
2024-08-10 22:07:58 -0400

    Figured out custom components and dynamic properties to begin the basis of factions

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,9 +1,28 @@
-import { world } from "@minecraft/server";
+import { ItemComponentUseEvent, world } from "@minecraft/server";
 import { NightVisionConsumeComponent } from "./NightVisionConsumeComponent";
+
+let doneLoading = false;
 
 world.beforeEvents.worldInitialize.subscribe((initEvent) => {
   initEvent.itemComponentRegistry.registerCustomComponent(
     "test_component:give_night_vision_on_consume",
     new NightVisionConsumeComponent()
   );
+
+  initEvent.itemComponentRegistry.registerCustomComponent("test_component:check_done_loading", {
+    onUse(arg: ItemComponentUseEvent) {
+      let factions = world.getDynamicPropertyIds();
+      factions.forEach((factionId) => {
+        let factionName = world.getDynamicProperty(factionId);
+        arg.source.sendMessage(factionId + ": " + factionName);
+      });
+    },
+  });
+
+  doneLoading = true;
+});
+
+world.afterEvents.worldInitialize.subscribe(() => {
+  world.setDynamicProperty("Faction 1", "Red");
+  world.setDynamicProperty("Faction 2", "Blue");
 });
